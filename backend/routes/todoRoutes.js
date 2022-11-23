4c8742f763f9a53943296a0ffd4272af6275790a
Ashish Jha <ashishjha14411@gmail.com>
2022-11-23 23:01:36 +0530

    changed getTodo route to home route

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -3,10 +3,8 @@ const express =require("express")
 const {createTodo, getTodo} = require("../controllers/todoControllers")
 const router = express.Router();
 
-router.post("/", (req,res) => {
-    res.send("HI HI HI")
-})
-router.post("/createTodo", createTodo)
-router.get("/getTodo", getTodo)
+router.get("/", getTodo)
+router.post("/createTodo", createTodo)/* 
+router.get("/", getTodo) */
 
 module.exports = router;
