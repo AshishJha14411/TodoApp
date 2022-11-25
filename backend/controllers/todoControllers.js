e2e070294819f25ac7bfb7ac4c0c03ed9f9cdc4c
Ashish Jha <ashishjha14411@gmail.com>
2022-11-25 23:10:53 +0530

    commented code redundancy

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -4,7 +4,7 @@ exports.createTodo = async (req,res) => {
     try {
     const {title, list} = req.body;
         const todo = await Todo.create({title, list});
-        await todo.save()
+        /* await todo.save() */
         return res.json(todo)
     } catch (error) {
         console.log(error)
