e4e39c57ac41411348ee31e4baee0e5eafe34a7d
Ashish Jha <ashishjha14411@gmail.com>
2022-11-25 23:11:36 +0530

    added login and register routes

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,10 +1,14 @@
 const express =require("express")
 
-const {createTodo, getTodo} = require("../controllers/todoControllers")
+const {createTodo, getTodo} = require("../controllers/todoControllers");
+const { register, login } = require("../controllers/userController");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", getTodo)
-router.post("/createTodo", createTodo)/* 
-router.get("/", getTodo) */
+/* router.get("/ge", getTodo) */
+router.post("/createTodo", auth, createTodo)
+router.get("/getTodo", auth, getTodo)
+router.post("/createUser", register)
+router.post("/login", login)
 
 module.exports = router;
