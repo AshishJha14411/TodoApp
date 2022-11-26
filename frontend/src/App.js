cd60a2e95b355dfac95ce214d1d2250280a535b5
Ashish Jha <ashishjha14411@gmail.com>
2022-11-26 22:03:05 +0530

    added tailwin config to index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import Home from "./components/Home";
 function App() {
   return (
-    <div className="App">
-   <h1>Hello World</h1>
+    <div className="">
+        <Home />
     </div>
   );
 }
