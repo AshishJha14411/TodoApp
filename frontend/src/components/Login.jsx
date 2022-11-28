9936b08ac89132459ea80a642f9e42a718126d1f
Ashish Jha <ashishjha14411@gmail.com>
2022-11-28 21:11:26 +0530

    added login components

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='bg-[#03203C] w-2/5 flex flex-col rounded-[4%] justify-center'>
-        <h1 className='text-[3rem] text-center font-bold text-white my-5'>Todo App Login</h1>
+        <h1 className='text-[3rem] text-center font-bold text-white my-5'>Login</h1>
         <input 
         className='w-3/5 mx-auto my-4 p-3'
         placeholder='Email Address'
