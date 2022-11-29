627334cbc3ad138ebb375ac2e69b532d574003dc
Ashish Jha <ashishjha14411@gmail.com>
2022-11-29 13:49:36 +0530

    modified cntrlers for new schema

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -1,11 +1,16 @@
-const Todo = require("../models/todoModel")
+const Todo = require("../models/todoModel");
+const usersModel = require("../models/usersModel");
 
 exports.createTodo = async (req,res) => {
     try {
-    const {title, list} = req.body;
-        const todo = await Todo.create({title, list});
+    const {title, task} = req.body;
+        const todo = await Todo.create({title, task});
         /* await todo.save() */
-        return res.json(todo)
+        res.status(201).json({
+            success: true,
+            message: "Todo created successfully",
+            todo
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send("server erroR")
@@ -17,5 +22,39 @@ exports.getTodo = async (req,res) => {
         return res.json(todo)
     } catch (error) {
         console.log(error)
+    }
+}
+
+exports.updateTodo = async (req,res) => {
+    try {
+        const todo = await Todo.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            success: true,
+            message: "Todo updated Sucessfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+exports.deleteTodo = async (req,res) => {
+    try {
+        const todoId = req.params.id;
+        const todo = await Todo.findByIdAndDelete(todoId);
+        res.status(200).json({
+            success: true,
+            message: "Todo Deleted Successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({
+            success: false,
+            message: error.message
+        })
     }
 }
