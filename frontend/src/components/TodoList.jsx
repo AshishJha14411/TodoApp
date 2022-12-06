6eaeb5243a445931ed2dd0420b132f443d1a22bf
Ashish Jha <ashishjha14411@gmail.com>
2022-12-06 18:35:25 +0530

    added todo list

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -2,23 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 const TodoList = () => {
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
 
   const fetchData = async () => {
     const resp = await axios.get("/getTodo");
-    setList(resp.data)
+
+    setList(resp.data);
     // if No users are there please dont set the value
-  };
-  console.log(list);
+  }; /* 
+  console.log(list); */
   useEffect(() => {
-    fetchData()
-  },[])
+    fetchData();
+  }, []);
   return (
     <>
-    
+      <div>
+        {list &&
+          list.map((todo) => (
+            <div key={todo._id}>
+              <ul>{todo.title}</ul>
+
+              <li>{todo.task}</li>
+            </div>
+          ))}
+      </div>
     </>
-  )
-  
+  );
 };
 
 export default TodoList;
