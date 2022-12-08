2587ae401fb8138699d6ff08357a4bb7c6641b45
Ashish Jha <ashishjha14411@gmail.com>
2022-12-08 21:47:13 +0530

    added add component

--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const AddTodo = () => {
-  return (
-    <div>AddTodo</div>
-  )
-}
 
-export default AddTodo
+  
+
+
+
+
+  return (
+    <div>
+      <input
+        className="w-full mx-auto my-4 p-3"
+        placeholder="Please Enter the Title"
+      />
+      <textarea className="w-full mx-auto my-4 p-3" />
+      <button className="ml-[45%] p-5 w-[10rem] text-white fond-extrabold bg-[#207398] rounded-full text-xl hover:bg-[#51E1ED] hover:text-black ease-in duration-200">
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+export default AddTodo;
