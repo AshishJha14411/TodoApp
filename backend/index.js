ae95eb618735127390dc68783c31691a407e5d35
Ashish Jha <ashishjha14411@gmail.com>
2023-02-07 11:12:45 +0530

    added CORS origin url and other param

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const { json } = require("express");
 const express = require("express");
 const connectToDB = require("./config/db");
+const cors = require("cors");
 const app = express();
-const todoRoutes = require("./routes/todoRoutes")
+
+const todoRoutes = require("./routes/todoRoutes");
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
 
 connectToDB();
-app.use("/", todoRoutes)
+app.use("/", todoRoutes);
 module.exports = app;
